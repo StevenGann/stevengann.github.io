1147b713581f532fb4bbb614bad27b0ec25ebd94
StevenGann <StevenGannJr@gmail.com>
2024-01-21 15:12:20 -0800

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt install ruby-full build-essential zlib1g-dev -y
+
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+gem install jekyll bundler
+
+
