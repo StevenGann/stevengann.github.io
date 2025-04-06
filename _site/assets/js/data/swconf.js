61b9a1602a3c9bdb6f878e9b084ea6ff775b38ad
Steven Gann <stevengannjr@gmail.com>
2025-04-06 14:32:26 -0700

    added avatar image

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743974409',resources: [
+    cacheName: 'chirpy-1743975130',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
