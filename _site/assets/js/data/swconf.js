0de0e46729b00448cb38481f980cc91c472009e4
Steven Gann <stevengannjr@gmail.com>
2025-04-06 16:46:46 -0700

    refreshed

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743977193',resources: [
+    cacheName: 'chirpy-1743983176',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
