ec17846cc7faddcc7d8a05e36f233d9ad60fe28e
Steven Gann <stevengannjr@gmail.com>
2025-04-06 18:24:08 -0700

    repair static generation

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1743988824',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/resume/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
